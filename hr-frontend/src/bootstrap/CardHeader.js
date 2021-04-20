d4e99cd87ec62a6607c6f5c2dd88744024b9f011
deepcloudlabs <deepcloudlabs@gmail.com>
2021-04-20 17:04:49 +0300

    initial commit

--- hr-frontend/src/bootstrap/CardHeader.js
+++ hr-frontend/src/bootstrap/CardHeader.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function CardHeader(props){
+function CardHeader(props) {
     return (
         <div className="card-header">
             <h3 className="card-title">{props.title}</h3>
